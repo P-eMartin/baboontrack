1a381f41c0742e65b1e2d00c5bd3eb3e9038b3d6
P-e <36887778+P-eMartin@users.noreply.github.com>
2026-05-26 23:19:34 +0200

    update file list

--- src/baboontrack/bt_utils/io_utils.py
+++ src/baboontrack/bt_utils/io_utils.py
@@ -226,7 +226,7 @@ def get_all_files_in_folder(folder_path, all_files=None, extensions=None):
         if entry.is_file() and (extensions is None or os.path.splitext(entry.name)[1].lower() in extensions):
             all_files.append(entry.path)
         elif entry.is_dir():
-            get_all_files_in_folder(entry.path, all_files, extensions)
+            all_files.extend(get_all_files_in_folder(entry.path, all_files, extensions))
     return all_files
 
 '''
