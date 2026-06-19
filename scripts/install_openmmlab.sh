594750cb105d69cdf512a839e57ead1e3c2e419c
P-e <36887778+P-eMartin@users.noreply.github.com>
2026-06-19 19:36:52 +0200

    workflow adapted

--- scripts/install_openmmlab.sh
+++ scripts/install_openmmlab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-uv pip install -U openmim "numpy<2.0"
+pip install -U openmim "numpy<2.0"
 # mim install mmengine==0.10.3
 mim install mmcv==2.1.0
-uv pip install torch==2.1.0 torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install torch==2.1.0 torchvision --index-url https://download.pytorch.org/whl/cu121
 mim install mmdet==3.2.0
 mim install mmpose==1.3.2
